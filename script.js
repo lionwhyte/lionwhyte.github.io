938edeb88b1fa2f8be7c9325e82b52adf175dbd2
lion <shiky69@protonmail.com>
2022-07-27 18:47:35 +0200

    location reminder v0.1

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
-const x = document.getElementById("demo");
+const location_name = document.getElementById("location_name"),
+    reminder_description = document.getElementById("reminder_description"),
+    latitude = document.getElementById("latitude"),
+    longitude = document.getElementById("longitude");
 
-function getLocation() {
+send_to_storage.onclick = () => {
+    if (location_name.value === "" || reminder_description.value === "" || latitude === "" || longitude === "") {
+        alert("Please fill all the input fields!");
+        return;
+    };
+    const reminder_object = {
+        reminder: reminder_description.value,
+        latitude: latitude.value,
+        longitude: longitude.value,
+    };
+    localStorage.setItem(location_name.value, JSON.stringify(reminder_object));
+    alert("Location reminder " + "'" + location_name.value + "'" + " saved.")
+}
+
+get_location.onclick = () => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition((position) => {
+            latitude.value = position.coords.latitude;
+            longitude.value = position.coords.longitude;
+        });
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
+        alert("Geolocation is not supported by this browser.");
+    };
+};
 
-function showPosition(position) {
-    if (position.coords.latitude < 44.266956 && position.coords.latitude > 44.266936) {
-        alert("You are at home!")
-    } else {
-        alert("You are not at home!")
-    }
-}
+let myInterval;
+run_app.onclick = () => {
+    if (myInterval !== undefined) {
+        return;
+    };
+    if (!navigator.geolocation) {
+        alert("Geolocation is not supported by this browser.");
+        return;
+    };
+    app_status.innerHTML = "Checking location.."
+    myInterval = setInterval(() => {
+        for (key in Object.keys(localStorage)) {
+            let storage_items = Object.keys(localStorage),
+                item = JSON.parse(localStorage.getItem(storage_items[key]))
+
+            navigator.geolocation.getCurrentPosition((position) => {
+                let lat = position.coords.latitude,
+                    long = position.coords.longitude,
+                    variation = 0.000010;
+
+                if ((lat + variation > item.latitude && lat - variation < item.latitude && long + variation > item.longitude && long - variation < item.longitude)) {
+                    alert("You are at " + "'" + storage_items[key] + "'.\n\nReminder: " + item.reminder)
+                    clearInterval(myInterval);
+                    myInterval = undefined;
+                    app_status.innerHTML = ""
+                };
+            });
+        };
+    }, 3000);
+};
+
+stop_app.onclick = () => {
+    if (myInterval !== undefined) {
+        clearInterval(myInterval);
+        myInterval = undefined;
+        app_status.innerHTML = ""
+    };
+}; 
