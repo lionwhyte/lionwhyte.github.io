a5d856103063d39ddc431ff62c7bc9f14b810324
lion <shiky69@protonmail.com>
2022-08-13 12:24:50 +0200

    pwd

--- script.js
+++ script.js
@@ -69,6 +69,21 @@ stop_app.onclick = () => {
     };
 };
 
+// Ensure that the browser supports the service worker API
+if (navigator.serviceWorker) {
+    // Start registration process on every page load
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            // The register function takes as argument
+            // the file path to the worker's file
+            .register('sw.js')
+            // Gives us registration object
+            .then(reg => console.log('Service Worker Registered'))
+            .catch(swErr => console.log(
+                `Service Worker Installation Error: ${swErr}}`));
+    });
+}
+
 function notifyMe(location, reminder) {
     navigator.serviceWorker.register('sw.js');
     Notification.requestPermission(function (result) {
